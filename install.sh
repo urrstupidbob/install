eeee6ab95591ec9e9b99c97b4fd68120b64fa8cc
urrstupidbob <98119050+urrstupidbob@users.noreply.github.com>
2023-03-01 21:35:10 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install the required packages
+
+sudo apt update && sudo apt upgrade && sudo apt install xfce4 xfce4-goodies && sudo apt install wine && sudo dpkg --add-architechture i386 && sudo apt update && sudo apt upgrade sudo apt install gdebi 
+
+
+
+# done!
+
+echo done!
